a1e2186273f5eac118af92965df29941a2962104
tejas <tejas@gmail.com>
2023-04-28 11:55:14 +0530

    push hector to github

---
+++ hector/src/test/java/com/mg/hector/HectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mg.hector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
